305c637f8e192085836a8925b4ec56a9694c8044
Xearta <rsmcwow@gmail.com>
2020-10-22 16:21:20 -0400

    Added Cards for destination previews

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import '../App.css';
+import Cards from '../components/Cards';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Cards />
     </>
   );
 };
