2a59070f59ab0b9160a1eab3b9ecd914c3ae3819
Xearta <rsmcwow@gmail.com>
2020-11-01 22:05:44 -0500

    Added header to booking form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './BookingForm.css';
 
 const BookingForm = () => {
+  //TODO: Handle the state for the active link
+  //TODO: Update the state on click of different link
+
   return (
     <div className='booking-form'>
-      <h1>Booking Form</h1>
+      <div className='booking-form-header'>
+        <li className='booking-form-item active'>
+          <Link to='#' className='booking-form-link'>
+            Build a trip
+          </Link>
+        </li>
+        <li className='booking-form-item'>
+          <Link to='#' className='booking-form-link'>
+            Hotels
+          </Link>
+        </li>
+        <li className='booking-form-item'>
+          <Link to='#' className='booking-form-link'>
+            Flights
+          </Link>
+        </li>
+        <li className='booking-form-item'>
+          <Link to='#' className='booking-form-link'>
+            Cars
+          </Link>
+        </li>
+        <li className='booking-form-item'>
+          <Link to='#' className='booking-form-link'>
+            Cruises
+          </Link>
+        </li>
+        <li className='booking-form-item'>
+          <Link to='#' className='booking-form-link'>
+            Things To Do
+          </Link>
+        </li>
+      </div>
     </div>
   );
 };
